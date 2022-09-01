152982fc549d5edb10adcae0459b8b1caf7acd08
Dana Reese <dana@danavalerie.com>
2022-09-01 13:44:25 +0700

    Fix compilation error

--- src/main/java/com/danavalerie/prompter/Sample.java
+++ src/main/java/com/danavalerie/prompter/Sample.java
@@ -11,7 +11,7 @@ package com.danavalerie.prompter;
 public class Sample {
 
     public static void main(String[] args) {
-        Prompter prompter = new Prompter(System.in);
+        Prompter prompter = new Prompter();
         int age = prompter.promptForInt("How old are you? ");
         int year = prompter.promptForInt("What year were you born? ");
         System.out.println("Then, we must be in the year " + (age + year) + " or " + (age + year + 1));
